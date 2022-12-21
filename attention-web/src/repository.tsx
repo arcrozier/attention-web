2229723e3aaef76139ce58bbd49c8900f5aa6165
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-12-20 22:56:35 -0800

    Worked on repository.tsx

--- attention-web/src/repository.tsx
+++ attention-web/src/repository.tsx
@@ -1,9 +1,14 @@
 import {UserInfo} from "./App";
+import axios from "./axios";
 
 const BASE_URL = process.env.BASE_URL
 
 // authentication info: https://medium.com/swlh/django-rest-framework-and-spa-session-authentication-with-docker-and-nginx-aa64871f29cd
 // https://www.django-rest-framework.org/api-guide/authentication/#sessionauthentication
 export async function getUserInfo(): Promise<UserInfo> {
-    throw Error
+    return axios.get(`${BASE_URL}/get_info/`)
+}
+
+export async function setCSRFToken(): Promise<any> {
+    return axios.get(`${BASE_URL}/set_csrf/`)
 }
