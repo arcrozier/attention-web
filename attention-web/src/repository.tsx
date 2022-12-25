9a8b316d11221d165cc025b288244adf84f94d14
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-12-24 16:06:07 -0800

    Added getting CSRF cookie

--- attention-web/src/repository.tsx
+++ attention-web/src/repository.tsx
@@ -1,18 +1,30 @@
 import {UserInfo} from "./App";
 import axios from "./axios";
+import {AxiosResponse} from "axios";
+import Cookies from "js-cookie";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL
+
+export const CSRF_COOKIE = 'csrftoken'
+
+async function requireCSRF() {
+    if (Cookies.get(CSRF_COOKIE) === undefined) await setCSRFToken()
+    return
+}
 
 // authentication info: https://medium.com/swlh/django-rest-framework-and-spa-session-authentication-with-docker-and-nginx-aa64871f29cd
 // https://www.django-rest-framework.org/api-guide/authentication/#sessionauthentication
 export async function getUserInfo(): Promise<UserInfo> {
+    await requireCSRF()
     return axios.get(`${BASE_URL}get_info/`)
 }
 
-export async function setCSRFToken(): Promise<any> {
+export async function setCSRFToken(): Promise<AxiosResponse> {
+    console.log('Getting CSRF Token')
     return axios.get(`${BASE_URL}set_csrf/`)
 }
 
 export async function login(username: string, password: string): Promise<any> {
+    await requireCSRF()
     return axios.post(`${BASE_URL}login_session/`, {username: username, password: password})
 }
