ac4d0f92d3dbfd85b4afdd1705e6ec7c69a5835a
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2023-06-17 23:50:58 -0700

    Started outlining settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,42 @@
 import {useTitle} from "../Root";
 import {useProps} from "../App";
-import {ReactElement} from "react";
 
-interface SettingProps {
-    icon?: ReactElement,
-    title: string
+interface PreferenceGroupProps {
+    title: string,
+    children: React.ReactNode
 }
 
-function Setting(props: SettingProps) {
+interface PreferenceProps<T> {
+    icon?: React.ReactNode,
+    title: string,
+    value: T,
+    action?: React.ReactNode,
+    summary?: (value: T) => string,
+    onClick?: () => void,
+    enabled?: boolean,
+    titleColor?: string,
+    summaryColor?: string,
+    disabledTitleColor?: string,
+    disabledSummaryColor?: string
+}
+
+interface DialogPreferenceProps<T> extends PreferenceProps<T> {
+    dialog: React.ReactNode,
+}
+
+function Preference<T>(props: PreferenceProps<T>) {
+
+}
+
+function DialogPreference<T>(props: DialogPreferenceProps<T>) {
+
+}
+
+function PreferenceGroup(props: {title: string, children: React.ReactNode}) {
+
+}
+
+function SplitPreference(props: {small: React.ReactNode, large: React.ReactNode}) {
 
 }
 
